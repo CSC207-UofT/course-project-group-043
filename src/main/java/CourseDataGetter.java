ee1808d5c8ee694b36c6bdd730fa94a1756bc3da
Rachel Xie <r.xie@mail.utoronto.ca>
2021-11-01 18:53:25 -0400

    Preliminary tests on getting course information from webpage

--- src/main/java/CourseDataGetter.java
+++ src/main/java/CourseDataGetter.java
@@ -1,3 +1,7 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -5,14 +9,18 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.List;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 
+// roughly followed tutorials from here: https://www.baeldung.com/java-9-http-client and
+// https://stackoverflow.com/questions/1359689/how-to-send-http-request-in-java
+
 public class CourseDataGetter {
 
-    public static void main (String [] args) throws URISyntaxException, IOException, InterruptedException {
+    public static void main (String [] args) throws URISyntaxException, IOException, InterruptedException, ParseException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://timetable.iit.artsci.utoronto.ca/api/20219/courses?code=csc"))
+                .uri(new URI("https://timetable.iit.artsci.utoronto.ca/api/20219/courses?code=csc")) //todo: make this last part a variable
                 .timeout(Duration.of(10, SECONDS))
                 .GET()
                 .build();
@@ -21,12 +29,22 @@ public class CourseDataGetter {
                 .build()
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+//        System.out.println(response.body()); // this prints it out nicely
+
+        JSONParser parser = new JSONParser();
+        JSONObject testObj = (JSONObject) parser.parse(response.body());
+
+//        System.out.println(testObj);
+
+//        System.out.println(testObj.get("CSC108H1-F-20219")); // this returns null, which it decidedly should not
+
+        JSONObject test2 = (JSONObject) parser.parse(String.valueOf(testObj));
+        System.out.println(test2.get("courseId")); // this does work. it does give you that specific object
     }
 
 }
 
-
+// this is the more old-fashioned method, but it better allows us to work with each individual line
 //import java.net.*;
 //import java.io.*;
 //
