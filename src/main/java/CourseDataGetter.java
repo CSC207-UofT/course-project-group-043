2591d0e11ed0269132afcf9be17f469624833afb
Rachel Xie <r.xie@mail.utoronto.ca>
2021-11-09 18:16:34 -0500

    Have a way to get and parse information from the website that works

--- src/main/java/CourseDataGetter.java
+++ src/main/java/CourseDataGetter.java
@@ -1,6 +1,6 @@
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONObject;
+// GOT THE ABOVE TO WORK USING THE LAST ANSWER ON HERE: https://stackoverflow.com/questions/34676940/importing-json-library-into-intellij-idea
+// potentially also need this, but not sure: https://mvnrepository.com/artifact/org.json/json/20140107
 
 import java.io.IOException;
 import java.net.URI;
@@ -9,7 +9,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.List;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 
@@ -18,7 +17,7 @@ import static java.time.temporal.ChronoUnit.SECONDS;
 
 public class CourseDataGetter {
 
-    public static void main (String [] args) throws URISyntaxException, IOException, InterruptedException, ParseException {
+    public static void main (String [] args) throws URISyntaxException, IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI("https://timetable.iit.artsci.utoronto.ca/api/20219/courses?code=csc")) //todo: make this last part a variable
                 .timeout(Duration.of(10, SECONDS))
@@ -29,17 +28,37 @@ public class CourseDataGetter {
                 .build()
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-//        System.out.println(response.body()); // this prints it out nicely
+        JSONObject allCourseData  = new JSONObject(response.body());
+//        System.out.println(jObject.get("CSC108H1-F-20219")); // it sure does print it but it's real ugly
+        JSONObject oneCourse = allCourseData.getJSONObject("CSC108H1-F-20219");
+        System.out.println(oneCourse.get("courseId")); // THIS PRINTS IT CORRECTLY
+//        System.out.println(oneCourse.getJSONObject("meetings"));
 
-        JSONParser parser = new JSONParser();
-        JSONObject testObj = (JSONObject) parser.parse(response.body());
+        if (oneCourse.get("courseId").equals("51814")) {
+            System.out.println("yep, that's the course code we wanted");
+        }
+
+//        System.out.println(response.body()); // this prints it out nicely
+//        System.out.println(response.body().getClass());  // this is for sure a java string
+
+        // all this is for json.simple library
+//        JSONParser parser = new JSONParser();
+//        JSONArray testObj = (JSONArray) parser.parse(response.body());
+//        System.out.println(testObj.get(0));
+
+
+//        String courseName = (String) testObj.get("CSC108H1-F-20219");
+//        System.out.println(courseName);
 
 //        System.out.println(testObj);
 
 //        System.out.println(testObj.get("CSC108H1-F-20219")); // this returns null, which it decidedly should not
 
-        JSONObject test2 = (JSONObject) parser.parse(String.valueOf(testObj));
-        System.out.println(test2.get("courseId")); // this does work. it does give you that specific object
+        // at minimum there are like 6 levels to this BS
+
+//        JSONObject test2 = (JSONObject) parser.parse(String.valueOf(testObj));
+        // don't know what that comment down there is about, this just gives you null
+//        System.out.println(test2.get("code")); // this does work. it does give you that specific object.
     }
 
 }
@@ -50,11 +69,11 @@ public class CourseDataGetter {
 //
 //public class CourseDataGetter {
 //    public static void main(String[] args) throws Exception {
-//        URL yahoo = new URL("https://timetable.iit.artsci.utoronto.ca/api/20219/courses?code=csc");
-//        URLConnection yc = yahoo.openConnection();
+//        URL timetableDataURL = new URL("https://timetable.iit.artsci.utoronto.ca/api/20219/courses?code=csc");
+//        URLConnection connectionTimetable = timetableDataURL.openConnection();
 //        BufferedReader in = new BufferedReader(
 //                new InputStreamReader(
-//                        yc.getInputStream()));
+//                        connectionTimetable.getInputStream()));
 //        String inputLine;
 //
 //        while ((inputLine = in.readLine()) != null)
