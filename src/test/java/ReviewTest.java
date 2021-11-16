814725b02c12e985c80018326179165f152c634b
XIFL <81833936+XIFL@users.noreply.github.com>
2021-11-15 22:35:51 -0500

    Added tests for review class.

---
+++ src/test/java/ReviewTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class ReviewTest {
+
+    @Test(timeout = 100)
+    public void Test1() {
+        Review review = new Review("Bahen Centre", "Great place for studying, and meeting new people.", 4);
+        String actual = review.printReview();
+        String expected = "Bahen Centre, 4/5.  Great place for studying, and meeting new people.";
+        assertEquals(expected, actual);
+    }
+
+    @Test(timeout = 100)
+    public void TestInvalidRating() {
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class, () -> {
+                    Review review = new Review("Bahen Centre", "Great place for studying, and meeting new people.", 6);
+                });
+        assertEquals("Rating 6 is not valid, rating must be between 0 and 5.", exception.getMessage());
+    }
+}
