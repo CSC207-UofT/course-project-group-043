c8fc898668791c434ba24bf296acf4fccf819ca4
dennis <dennis.tat@mail.utoronto.ca>
2021-12-07 21:11:22 -0500

    updated timelist

--- src/main/java/FrameworksDrivers/ScheduleDrawing.java
+++ src/main/java/FrameworksDrivers/ScheduleDrawing.java
@@ -65,9 +65,9 @@ public class ScheduleDrawing extends JComponent {
         Color[] colorsList = {color0, color1, color2, color3, color4, color5, color6, color7};
 
         String[] titles = {"Time", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-        String[] timesList = {"12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00",
-                "8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00",
-                "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00"};
+        String[] timesList = {"00:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00",
+                "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00",
+                "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"};
 
 
         // drawing the day of week header boxes & outlines
