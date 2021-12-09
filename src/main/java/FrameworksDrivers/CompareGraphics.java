f9c85358a00a605271de03270e6c9d7609ed77f0
Rachel Xie <r.xie@mail.utoronto.ca>
2021-12-08 20:15:12 -0500

    Added documentation for frameworks & drivers classes

--- src/main/java/FrameworksDrivers/CompareGraphics.java
+++ src/main/java/FrameworksDrivers/CompareGraphics.java
@@ -13,6 +13,9 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Uses swing to show results of comparing schedules
+ */
 public class CompareGraphics extends JFrame implements ActionListener {
     private Graphics g2g;
     private final AccountManager accountManager;
@@ -23,6 +26,16 @@ public class CompareGraphics extends JFrame implements ActionListener {
     private final String[] timesList = {"00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"};
 
 
+    /**
+     * Creates a new instance of CompareGraphics
+     *
+     * @param user1 The first user whose schedule will be compared
+     * @param user2 The second user whose schedule will be compared
+     * @param accountManager An instance of AccountManager, used to perform the comparison
+     * @throws IOException
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public CompareGraphics(String user1, String user2, AccountManager accountManager)
             throws IOException, ExecutionException, InterruptedException {
         super("Schedule Compare");
